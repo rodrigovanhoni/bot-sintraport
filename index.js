d4d72ec992818b1a8953eac9fc5acea5bad02372
Rodrigo Vanhoni <36582701-rodrigovanhoni@users.noreply.replit.com>
2024-11-23 19:13:45 +0000

    Ajustes

--- index.js
+++ index.js
@@ -229,6 +229,7 @@
         });
     });
 
-    app.listen(port, '0.0.0.0', () => {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
         console.log(`Servidor rodando na porta ${port}`);
     });
